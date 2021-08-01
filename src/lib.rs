05124d24fe716e7a703e46aa8ead5d2925672dfa
sotatek <quang.le@sotatek.com>
2021-08-02 00:50:02 +0700

    + add tx

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 pub mod block;
 pub mod hashable;
 pub mod blockchain;
+pub mod transaction;
 
 pub use crate::block::Block;
 pub use crate::hashable::Hashable;
 pub use crate::blockchain::Blockchain;
+pub use crate::transaction::Transaction;
+use std::time::{ SystemTime, UNIX_EPOCH };
 
+type Hash = Vec<u8>;
+type Address = String;
 type BlockHash = Vec<u8>;
 
 pub fn u32_bytes (u: &u32) -> [u8; 4] {
@@ -38,4 +43,12 @@ pub fn difficulty_bytes_as_u128 (v: &Vec<u8>) -> u128 {
         ret = ret | ((v[i] as u128) << (8 * (i - 16)));
     }
     ret
+}
+
+pub fn now () -> u128 {
+    let duration = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap();
+
+    duration.as_secs() as u128 * 1000 + duration.subsec_millis() as u128
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,69 @@
 use blockchainlib::*;
+use std::time::SystemTime;
 
 fn main() {
     let difficulty = 0x00ffffffffffffffffffffffffffffff;
 
     let mut genesis_block = Block::new(
         0,
-        0,
+        now(),
         vec![0; 32],
-        0,
-        "Genesis block!".to_owned(),
+        vec![
+            Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 50,
+                    },
+                    transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 30,
+                    }
+                ]
+            }
+        ],
         difficulty
     );
 
     genesis_block.mine();
-    println!("{:?}", genesis_block);
+    println!("Mined genesis block: {:?}", genesis_block);
 
     let mut last_hash = genesis_block.hash.clone();
+    let mut blockchain = Blockchain::new();
 
-    let chain = Blockchain {
-        blocks: vec![genesis_block]
-    };
+    blockchain.update_with_block(genesis_block).expect("Fail to add genesis block!");
 
-    for i in 1..=10 {
-        let mut new_block = Block::new(
-            i,
-            0,
-            last_hash,
-            0,
-            "Not genesis block!".to_owned(),
-            difficulty
-        );
-        new_block.mine();
-        println!("{:?}", new_block);
+    let mut block_1 = Block::new(
+        1,
+        now(),
+        last_hash,
+        vec![
+            Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Miner_Quang".to_owned(),
+                        value: 1000
+                    }
+                ]
+            },
+            Transaction {
+                inputs: vec![
+                    blockchain.blocks[0].transactions[0].outputs[0].clone()
+                ],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Christ".to_owned(),
+                        value: 49,
+                    },
+                ]
+            }
+        ],
+        difficulty
+    );
 
-        last_hash = new_block.hash.clone();
-    }
-    println!("{}", chain.verify());
+    block_1.mine();
+    println!("Mined block 1: {:?}", block_1);
+    blockchain.update_with_block(block_1).expect("Fail to add block_1");
 }
